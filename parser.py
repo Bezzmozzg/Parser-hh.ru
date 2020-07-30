e340cadf226360a32800d07d323ad1e1dd8d8de9
Bezzmozzg <38427951+Bezzmozzg@users.noreply.github.com>
2020-07-30 13:37:20 +0500

    final

--- parser.py
+++ parser.py
@@ -16,17 +16,20 @@ headers = {
                   "Safari/537.36"}
 
 result = []
+name, worker_url, company_url, price, description = '','','','',''
 
 page = requests.get(url, params=payload, headers=headers)
 
 soup = BeautifulSoup(page.text, "html.parser")
 
 for i in soup.findAll('div', class_='vacancy-serp-item'):
-    result.append(["Name: " + i.find('a', class_="bloko-link HH-LinkModifier").text,
-                   'url: ' + i.find('a', class_="bloko-link HH-LinkModifier").get('href'),
-                   'company_url: https://perm.hh.ru' + i.find('a', class_="bloko-link bloko-link_secondary").get('href'),
-                   'price: ' + i.find('div', class_='vacancy-serp-item__sidebar').text,
-                   'description: ' + i.find('div', class_='g-user-content').text])
+    name = "Name: " + i.find('a', class_="bloko-link HH-LinkModifier").text
+    worker_url = 'Url: ' + i.find('a', class_="bloko-link HH-LinkModifier").get('href')
+    company_url = 'Company_url: https://perm.hh.ru' + i.find('a', class_="bloko-link bloko-link_secondary").get('href')
+    price = 'Price: ' + i.find('div', class_='vacancy-serp-item__sidebar').text
+    if price == 'Price: ': price = 'Price: None'
+    description = 'Description: ' + i.find('div', class_='g-user-content').text
+    result.append([name, worker_url, company_url, price, description])
 
 with open('result.json', 'w') as f:
     json.dump([result], f, ensure_ascii=False, indent=1)
